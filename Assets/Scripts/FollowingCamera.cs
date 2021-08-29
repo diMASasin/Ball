a4bdfd045a7b481579feb5d1c03eb7f05bfdc522
diMASasin <35368058+diMASasin@users.noreply.github.com>
2021-08-29 15:53:43 +0500

    fixes 5

--- Assets/Scripts/FollowingCamera.cs
+++ Assets/Scripts/FollowingCamera.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class FollowingCamera : MonoBehaviour
 {
+    [SerializeField] private Camera _camera;
     [SerializeField] private Transform _target;
     [SerializeField] private Vector3 _offset = new Vector3(0, 2, -10);
 
     private void Update()
     {
-        Camera.main.transform.position = _target.position + _offset;
+        _camera.transform.position = _target.position + _offset;
     }
 }
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -13,29 +13,31 @@ public class LevelGenerator : MonoBehaviour
 
     private void Update()
     {
-        GenerateLevelInRadius(_spawnRadius, (int)_player.transform.position.x);
+        GenerateLevelInRadius(_player.transform.position, _spawnRadius);
     }
 
-    private void GenerateLevelInRadius(int radius, int playerX)
+    private void GenerateLevelInRadius(Vector3 center, int radius)
     {
-        for (int i = playerX; i < playerX + radius; i++)
+        for (int i = (int)center.x; i < (int)center.x + radius; i++)
         {
             GenerateGridObject(new Vector3(i, (float)GridLayer.Ground, 0));
-            if(i != playerX)
+            if(i != (int)center.x)
                 GenerateGridObject(new Vector3(i, (float)GridLayer.OnGround, 0));
         }
     }
 
     private void GenerateGridObject(Vector3 spawnPoint)
     {
-        if (_occupiedPositions.Contains(WorldToGrid(spawnPoint)))
-            return;
-
-        _occupiedPositions.Add(WorldToGrid(spawnPoint));
-
         int templateIndex = GetTemplateIndex(spawnPoint);
         if (templateIndex < 0)
+        {
+            _occupiedPositions.Add(WorldToGrid(spawnPoint));
             return;
+        }
+
+        for (int i = 0; i < _templates[templateIndex].Quantity; i++)
+            if (_occupiedPositions.Contains(WorldToGrid(spawnPoint + new Vector3(i, 0, 0))))
+                return;
 
         for (int i = 0; i < _templates[templateIndex].Quantity; i++)
         {
